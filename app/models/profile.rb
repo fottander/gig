27b13a90866a901b870afa655afb3cc97aee3aa3
felixottander <felix.ottander@yahoo.com>
2017-10-24 16:55:49 +0200

    Adds dropdown menu in categories and cities options all over page.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,12 @@ class Profile < ApplicationRecord
   validates_presence_of :username, :description, :category, :city
   belongs_to :user
   has_many :applications, dependent: :destroy
+
+  def self.city
+    ['Hela sverige', 'Göteborg', 'Malmö', 'Stockholm']
+  end
+
+  def self.category
+    ['Målare', 'Snickare', 'Plåtslagare', 'Lagerarbetare']
+  end
 end
