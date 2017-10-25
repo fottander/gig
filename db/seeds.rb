c4acbb72a4c3910294959a2a2a22396b2b7254e0
felixottander <felix.ottander@yahoo.com>
2017-10-25 17:52:56 +0200

    modifies seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 User.delete_all
+Company.delete_all
 
 User.create(
   email: "felix.ottander@yahoo.com",
@@ -8,4 +9,17 @@ User.create(
   password: "123456"
   )
 
+Company.create(
+  email: "info@buildingblockz.se",
+  name: "Billnodix",
+  username: "Anders p",
+  address: "Södra gatan 33",
+  zip_code: "41267",
+  city: "Göteborg",
+  org_number: "3423443453",
+  phone: "079347238723",
+  password: "123456"
+)
+
+puts "Created #{Company.count} company"
 puts "Created #{User.count} user"
