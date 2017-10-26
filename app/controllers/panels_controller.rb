cbfb522a78b790ac1697ad631db40f5a47ffb3a3
felixottander <felix.ottander@yahoo.com>
2017-10-26 17:31:00 +0200

    Adds active jobs in dash and panel.

--- app/controllers/panels_controller.rb
+++ app/controllers/panels_controller.rb
@@ -2,5 +2,6 @@ class PanelsController < ApplicationController
   before_action :authenticate_company!
   def index
     @job = Job.where(company_id: current_company)
+    @application = Application.where(job_id: @job)
   end
 end
