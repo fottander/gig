08c09424858f18c51a7589e98a09d56584c86613
felixottander <felix.ottander@yahoo.com>
2017-11-08 10:28:10 +0100

    Modifeis invoice factory

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :invoice do
-    quantity 1
-    unit 1
-    amount "MyString"
+    quantity "123"
+    unit "123"
+    amount 12345
     first_day "2017-10-30"
     last_day "2017-10-30"
     description "MyText"
