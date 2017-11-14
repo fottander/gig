b555b986e03547f1c47857aa97e4e2f641f3f41c
felixottander <felix.ottander@yahoo.com>
2017-11-14 15:56:02 +0100

    Adds truncate on too long job title in breadcrumbs

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -7,7 +7,7 @@ class ApplicationsController < ApplicationController
     @profile = Profile.find_by(user_id: current_user)
     add_breadcrumb 'Start', :root_path
     add_breadcrumb 'Hitta Jobb', :jobs_path
-    add_breadcrumb "#{@job.title}", job_path(@job)
+    add_breadcrumb "#{@job.title}".truncate(24), job_path(@job)
     add_breadcrumb 'Skapa Ansökan'
   end
 
@@ -18,7 +18,7 @@ class ApplicationsController < ApplicationController
     @profile = Profile.find_by(user_id: current_user)
     add_breadcrumb 'Start', :root_path
     add_breadcrumb 'Hitta Jobb', :jobs_path
-    add_breadcrumb "#{@job.title}", job_path(@job)
+    add_breadcrumb "#{@job.title}".truncate(24), job_path(@job)
     add_breadcrumb "ID #{@job.id}"
   end
 
