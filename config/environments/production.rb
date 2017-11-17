3777152f31d6449bd15e27f8422baecad66f88c0
felixottander <felix.ottander@yahoo.com>
2017-11-17 13:52:21 +0100

    fis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "gig_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'gigalot.herokuapp.com' }
+
   config.action_mailer.perform_deliveries = false
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
