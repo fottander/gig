b72fe0d8d82e623c6e77a31af7238ad25537f919
felixottander <felix.ottander@yahoo.com>
2017-12-01 18:55:20 +0100

    Adds some data in administration

--- app/controllers/administrations_controller.rb
+++ app/controllers/administrations_controller.rb
@@ -2,5 +2,7 @@ class AdministrationsController < ApplicationController
   before_action :authenticate_admin!
   def index
     @categories = Category.all
+    @invoices = Invoice.last(10)
+    @jobs = Job.last(10)
   end
 end
