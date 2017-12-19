5bd966bb471960a0c50cecdf3effd362b5f72862
fottander <felix.ottander@yahoo.com>
2017-12-19 15:14:10 +0100

    New nav style (#77)
    * Adds new nav bar styling.
    
    * Removes javascript for old nav bar.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,13 +18,3 @@
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
-
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myNav() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
