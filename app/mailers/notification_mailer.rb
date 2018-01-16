f95a86db569a392645cceee9a436df9b95be42c5
felixottander <felix.ottander@yahoo.com>
2018-01-16 15:12:27 +0100

    Changes some invoice id numbers to uuid compatible

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -7,7 +7,7 @@ class NotificationMailer < ApplicationMailer
 
     mail(
     to: @user.email,
-    subject: "Din faktura med id: #{invoice.id} har blivit godkänd"
+    subject: "Din faktura med id: #{invoice.id.first(8)} har blivit godkänd"
     )
   end
 
@@ -17,7 +17,7 @@ class NotificationMailer < ApplicationMailer
 
     mail(
     to: @company.email,
-    subject: "Ny faktura har skickats till er med id: #{invoice.id}"
+    subject: "Ny faktura har skickats till er med id: #{invoice.id.first(8)}"
     )
   end
 
