8dd145301e8cfe487f4533f4020f9c2b057d6389
fottander <felix.ottander@yahoo.com>
2018-01-17 10:39:28 +0100

    Adds filtering for more attributes (#89)

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :pers_num, :email]
+Rails.application.config.filter_parameters += [:password, :pers_num, :bank, :clear_nr, :account_nr, :email, :phone, :org_number, :company_email, :ocr_number, :amount]
