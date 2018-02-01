ac80b3b62b0104549fd78ec1c783851abfe97299
felixottander <felix.ottander@yahoo.com>
2018-02-01 18:33:02 +0100

    Adds load pagination in administrations.

--- app/controllers/administrations_controller.rb
+++ app/controllers/administrations_controller.rb
@@ -1,9 +1,9 @@
 class AdministrationsController < ApplicationController
   before_action :authenticate_admin!
   def index
-    @categories = Category.all
-    @invoices = Invoice.not_active.paginate(page: params[:page])
-    @ezinvoices = Ezinvoice.not_active.paginate(page: params[:page])
+    @categories = Category.all.paginate(page: params[:categories], per_page: 4)
+    @invoices = Invoice.not_active.paginate(page: params[:invoices])
+    @ezinvoices = Ezinvoice.not_active.paginate(page: params[:ezinvoices])
     @jobs = Job.last(10)
     @profiles = Profile.last(10)
     @companies = Company.last(10)
