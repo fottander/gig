3a94c352e0f1935a59aa616131817d054e2793f2
felixottander <felix.ottander@yahoo.com>
2018-02-01 18:08:25 +0100

    Adds load pagination in panels

--- app/controllers/panels_controller.rb
+++ app/controllers/panels_controller.rb
@@ -1,9 +1,11 @@
 class PanelsController < ApplicationController
   before_action :authenticate_company!
   def index
-    @jobs = Job.where(company_id: current_company)
+    @jobs = Job.where(company_id: current_company).paginate(page: params[:jobs], per_page: 4)
     @applications = Application.where(job_id: @jobs)
-    @invoices = Invoice.where(company_id: current_company.id)
+    @applications_hired = @applications.hired.paginate(page: params[:hired])
+    @applications_complete = @applications.complete.paginate(page: params[:complete])
+    @invoices = Invoice.where(company_id: current_company.id).paginate(page: params[:invoices], per_page: 4)
     @activities = PublicActivity::Activity.order('created_at desc').where(recipient_id: current_company.id, recipient_type: 'Company')
   end
 end
