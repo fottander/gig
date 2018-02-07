0638dcc4f6d40eea91d6d5922a937e2b3b780744
felixottander <felix.ottander@yahoo.com>
2018-02-07 15:04:07 +0100

    Mailer enabled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,13 +62,26 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "gig_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'gigalot.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'joboy.se' }
 
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.perform_deliveries = true
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "joboy.se",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
