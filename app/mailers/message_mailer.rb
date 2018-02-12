541ddc27da0a1d52ffee0d13f45b6856b4aab5af
felixottander <felix.ottander@yahoo.com>
2018-02-12 21:59:06 +0100

    Adds views html and text for message mailer

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,13 +1,13 @@
+require 'digest/sha2'
 class MessageMailer < ApplicationMailer
   # use your own email address here
   default to: 'info@anewbiz.se'
-  default sender: 'info@anewbiz.se'
   default subject: 'Support'
   default "Message-ID"=>"<#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@anewbiz.se>"
 
   def message_me(msg)
     @msg = msg
-
-    mail(from: @msg.email, name: @msg.name, phone_number: @msg.phone_number, body: @msg.body)
+    @body = msg.body
+    mail(from: @msg.email, name: @msg.name, phone_number: @msg.phone_number)
   end
 end
