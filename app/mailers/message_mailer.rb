6320d2501cfe75c71307bbad46bd1aac0c063058
felixottander <felix.ottander@yahoo.com>
2018-02-12 20:31:35 +0100

    Adds api mailgun settings

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,14 +1,17 @@
 class MessageMailer < ApplicationMailer
-  # use your own email address here
-  default to: 'info@anewbiz.se'
-  default sender: 'info@anewbiz.se'
-  default from: 'info@anewbiz.se'
-  default subject: 'Support'
-  default "Message-ID"=>"<#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@anewbiz.se>"
+  require 'mailgun'
 
   def message_me(msg)
     @msg = msg
-
-    mail(email: @msg.email, name: @msg.name, phone_number: @msg.phone_number, body: @msg.body)
+    mg_client = Mailgun::Client.new ENV['mailgun_api']
+    message_params = {from: @msg.email,
+                      to: 'info@anewbiz.se',
+                      sender: 'info@anewbiz.se',
+                      subject: 'Support',
+                      message_id: "<#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@anewbiz.se>",
+                      name: @msg.name,
+                      phone_number: @msg.phone_number,
+                      text: @msg.body}
+    mg_client.send_message ENV['domain'], message_params
   end
 end
