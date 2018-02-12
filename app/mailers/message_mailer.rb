8e5c4f31c7e010c6436c02664548dd8d58e53282
felixottander <felix.ottander@yahoo.com>
2018-02-12 22:17:41 +0100

    Testing

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,13 +1,13 @@
 require 'digest/sha2'
 class MessageMailer < ApplicationMailer
   # use your own email address here
-  default to: 'info@anewbiz.se'
+  default from: 'info@anewbiz.se'
   default subject: 'Support'
   default "Message-ID"=>"<#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@anewbiz.se>"
 
   def message_me(msg)
     @msg = msg
     @body = msg.body
-    mail(from: @msg.email, name: @msg.name, phone_number: @msg.phone_number)
+    mail(to: @msg.email, name: @msg.name, phone_number: @msg.phone_number)
   end
 end
