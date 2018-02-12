cbfe5838594d224386880400030a6894636bdf0b
felixottander <felix.ottander@yahoo.com>
2018-02-12 18:24:23 +0100

    Adds from and sender in contact and usees the email field instead for sender email

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,12 +1,14 @@
 class MessageMailer < ApplicationMailer
   # use your own email address here
   default to: 'info@anewbiz.se'
+  default sender: 'info@anewbiz.se'
+  default from: 'info@anewbiz.se'
   default subject: 'Support'
   default "Message-ID"=>"<#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@anewbiz.se>"
 
   def message_me(msg)
     @msg = msg
 
-    mail(from: @msg.email, name: @msg.name, phone_number: @msg.phone_number, body: @msg.body)
+    mail(email: @msg.email, name: @msg.name, phone_number: @msg.phone_number, body: @msg.body)
   end
 end
