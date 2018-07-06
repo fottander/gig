1c8638899abac6a029946dbe636a5013daa2de83
felixottander <felix.ottander@yahoo.com>
2018-07-06 15:00:15 +0200

    Adds intergity plicy page

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,8 @@ Rails.application.routes.draw do
 
   resources :terms_of_services, only: [:index]
 
+  resources :integritypolicies, only: [:index]
+
   resources :users, only: [:index, :edit, :update, :destroy]
 
   resources :users_without_profiles, only: [:index]
