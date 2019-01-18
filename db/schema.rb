41133a11bc6899c5ec0b0ed46f3b99fbbf2857ad
felixottander <felix.ottander@yahoo.com>
2019-01-18 11:34:38 +0100

    removes schema shift

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_17_184536) do
+ActiveRecord::Schema.define(version: 2019_01_17_162833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -296,17 +296,6 @@ ActiveRecord::Schema.define(version: 2019_01_17_184536) do
     t.datetime "avatar_updated_at"
     t.date "age"
     t.index ["user_id"], name: "index_profiles_on_user_id"
-  end
-
-  create_table "shifts", force: :cascade do |t|
-    t.date "start_date"
-    t.date "end_date"
-    t.time "start_time"
-    t.time "end_time"
-    t.uuid "invoice_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["invoice_id"], name: "index_shifts_on_invoice_id"
   end
 
   create_table "users", force: :cascade do |t|
