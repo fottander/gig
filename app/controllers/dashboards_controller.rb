58f873ac78bce3770aafd5357f06d4f2b319e5d7
felixottander <felix.ottander@yahoo.com>
2019-02-09 17:33:25 +0100

    Test design for dashboard scrolling of footer

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -7,5 +7,6 @@ class DashboardsController < ApplicationController
     @applications_complete = @applications.complete.paginate(page: params[:complete])
     @invoices = Invoice.where(user_id: current_user).paginate(page: params[:invoices])
     @activities = PublicActivity::Activity.order('created_at desc').where(recipient_id: current_user.id, recipient_type: 'Profile').includes(:owner, :trackable).first(10)
+    render layout: 'mobile_scroll_layout'
   end
 end
