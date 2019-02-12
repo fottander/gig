0b4b1173e0e04008920f4849059c2cbda654827d
fottander <felix.ottander@yahoo.com>
2019-02-12 13:57:57 +0100

    Default footer design (#165)
    * Ojioj
    
    * removes footer from useful links
    
    * Design contact

--- app/controllers/our_rates_controller.rb
+++ app/controllers/our_rates_controller.rb
@@ -2,5 +2,6 @@ class OurRatesController < ApplicationController
   def index
     add_breadcrumb 'Start', :root_path
     add_breadcrumb 'Våra avgifter'
+    render layout: 'mobile_scroll_layout'
   end
 end
--- app/controllers/why_us_controller.rb
+++ app/controllers/why_us_controller.rb
@@ -2,5 +2,6 @@ class WhyUsController < ApplicationController
   def index
     add_breadcrumb 'Start', :root_path
     add_breadcrumb 'Varför Qnekt?'
+    render layout: 'mobile_scroll_layout'
   end
 end
